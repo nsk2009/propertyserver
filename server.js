3bfc2a19f3b926a010fbb64cafa72e77674a53af
Karthikeyan <nskarthi@aparajayah.com>
2022-09-22 20:09:14 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -355,7 +355,7 @@ app.post('*', function(req, res){
   res.status(404).send({ message : "Page not found!"});
 });
 // set port, listen for requests
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3050
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
