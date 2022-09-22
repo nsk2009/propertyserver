b370151a4bc975f8ef35a9ae8c8b75054e2a1664
Karthikeyan <nskarthi@aparajayah.com>
2022-09-22 20:44:15 +0530

    Port Changed

--- server.js
+++ server.js
@@ -355,7 +355,7 @@ app.post('*', function(req, res){
   res.status(404).send({ message : "Page not found!"});
 });
 // set port, listen for requests
-const PORT = process.env.PORT || 3050
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
