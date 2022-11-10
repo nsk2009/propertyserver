a8d315dc8039f64d632999189a996a207925cd1d
Karthikeyan <nskarthi@aparajayah.com>
2022-11-10 19:06:41 +0530

    DBconfig

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-//url: "mongodb+srv://moolah:9Id6bn87U4CeERwT@cluster0.e67br.mongodb.net/property?retryWrites=true&w=majority"
-url: "mongodb://127.0.0.1:27017/tradify" 
+url: "mongodb+srv://moolah:9Id6bn87U4CeERwT@cluster0.e67br.mongodb.net/property?retryWrites=true&w=majority"
+//url: "mongodb://127.0.0.1:27017/tradify" 
 //url: "mongodb+srv://moolah:9Id6bn87U4CeERwT@cluster0.e67br.mongodb.net/moolahstaging?retryWrites=true&w=majority"
 };
