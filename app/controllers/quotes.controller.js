b6a260f592f1e231bc557a4a57950564a6574ad1
Aparajayah <mariselvam@aparajayah.com>
2022-12-19 13:35:11 +0530

    pdf issue

--- app/controllers/quotes.controller.js
+++ app/controllers/quotes.controller.js
@@ -468,7 +468,7 @@ const generatePdf = async(id) => {
 	  /*await page.goto('https://news.ycombinator.com', {
 		waitUntil: 'networkidle2',
 	  });*/
-	  await page.setContent('Test PDF dfdsghfds', { waitUntil: ['domcontentloaded', 'networkidle2'] });
+	  await page.setContent('Test PDF', { waitUntil: ['domcontentloaded', 'networkidle2'] });
 	  // page.pdf() is currently supported only in headless mode.
 	  // @see https://bugs.chromium.org/p/chromium/issues/detail?id=753118
 	  await page.pdf({
