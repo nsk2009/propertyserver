ff0618363602ab0e00f4c7824e08ebaa93c51544
Aparajayah <mariselvam@aparajayah.com>
2022-12-19 15:08:51 +0530

    pdf issue

--- app/controllers/quotes.controller.js
+++ app/controllers/quotes.controller.js
@@ -474,7 +474,7 @@ const generatePdf = async(id) => {
 	  // page.pdf() is currently supported only in headless mode.
 	  // @see https://bugs.chromium.org/p/chromium/issues/detail?id=753118
 	  await page.pdf({
-		path: './quotes/test.pdf',
+		path: `./quotes/${id}.pdf`,
 		format: 'letter',
 		displayHeaderFooter:true,
 		footerTemplate: foot,
